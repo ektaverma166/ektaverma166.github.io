158a67c1998e30c7d2487cc0037323740ae7c383
Ekta Verma <ektaverma166@gmail.com>
2019-10-31 23:36:07 -0400

    Update script_home.js

--- homework_6/homework_6b/script_home.js
+++ homework_6/homework_6b/script_home.js
@@ -1,17 +1,18 @@
 function onLoad(){
-  var cart_quantity = parseInt(localStorage.getItem("cart_quantity"));
+  var cart_quantity = localStorage.getItem("cart_quantity");
   //cart_quantity = null;
   if (cart_quantity === null) 
   {
     localStorage.setItem("cart_quantity", 0);
-    document.getElementById("cart_number").textContent = cart_quantity;
+    document.getElementById("cart_number").textContent = 0;
     var cart=[];
     localStorage.setItem("cart", JSON.stringify(cart));
-    alert("cart null");
+    console.log("cart null");
   }
   
   else
   {
     document.getElementById("cart_number").textContent = cart_quantity;
+    console.log("cart not null");
   }
 }
