4da39805cab6b38c6254a439803a15a746392c90
Ekta Verma <ektaverma166@gmail.com>
2019-10-31 23:36:09 -0400

    Update script_products.js

--- homework_6/homework_6b/script_products.js
+++ homework_6/homework_6b/script_products.js
@@ -1,13 +1,13 @@
 function onLoad(){
-  var cart_quantity = parseInt(localStorage.getItem("cart_quantity"));
+  var cart_quantity = localStorage.getItem("cart_quantity");
   //cart_quantity = null;
   if (cart_quantity === null) 
   {
     localStorage.setItem("cart_quantity", 0);
-    document.getElementById("cart_number").textContent = cart_quantity;
+    document.getElementById("cart_number").textContent = 0;
     var cart=[];
     localStorage.setItem("cart", JSON.stringify(cart));
-    alert("cart null");
+    console.log("cart null");
   }
   
   else
